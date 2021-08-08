d0a07320249699890840cf04814134c69eda35b7
NicholasLeone <nicholasjinleone@gmail.com>
2021-08-08 00:07:11 -0400

    Adding back advoard, pozyx, plotjuggler ros plugins and plotjuggler msgs packages

---
+++ advoard_localization/src/position_error.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+'''
+__author__ = "Nicholas Leone"
+__email__ = "nicholasjinleone@gmail.com"
+__email__ = "nleone6@gatech.edu"
+'''
+
+'''
+Absolute Error of Chan Ho and Harbi Position Calculation
+
+Subscribes to localization_data_chan_ho_topic, localization_data_harbi_topic, and gazebo/model_states.
+
+Calculates error between the calculated position and the actual position of the Roomba for each localization algorithm.
+'''
+
+
+from logging import error
+
+from scipy.linalg.decomp_svd import null_space
+import rospy
+from pozyx_simulation.msg import  uwb_data
+from geometry_msgs.msg import Pose
+from gazebo_msgs.msg import ModelStates
+
+import tf
+import time
+import math
+
+import numpy as np
+import matplotlib.pyplot as plt 
+import math
+from scipy.linalg import eigvals
+import re
+
+all_distance = []
+all_destination_id = []
+pose_x = 0 
+pose_y = 0
+
+global sensor_pos
+sensor_pos = []
+base_K = 0
+global r_coefficient, constant, c, counter
+r_coefficient = []
+constant = []
+counter = 0
+c = 0.299792458
+
+global robot_pose_x,robot_pose_y,robot_pose_z
+robot_pose_x =0
+robot_pose_y =0
+robot_pose_z =0
+
+rospy.init_node('position_error_node', anonymous=True)
+pub_chan_ho = rospy.Publisher('position_error_chan_ho_topic', Pose, queue_size=10)
+pub_harbi = rospy.Publisher('position_error_harbi_topic', Pose, queue_size=10)
+
+
+def subscribe_data_robot_poses(ModelStates):
+    #for the get real position of robot subscribe model states topic
+    global robot_pose_x, robot_pose_y, robot_pose_z
+    global counter
+    counter = counter + 1
+
+    #gazebo/modelstate topic frequency is 100 hz. We descrese 10 hz with log method
+    if counter % 100 == 0:
+        counter = 0
+
+        #ModelStates.pose[2] = turtlebot3 model real position on modelstates
+        #robot_pose_x =ModelStates.pose[MODELSTATE_INDEX].position.x*1000
+        #robot_pose_y =ModelStates.pose[MODELSTATE_INDEX].position.y*1000
+        #robot_pose_z =ModelStates.pose[MODELSTATE_INDEX].position.z*1000
+
+        robot_pose_x = ModelStates.pose[MODELSTATE_INDEX].position.x
+        robot_pose_y = ModelStates.pose[MODELSTATE_INDEX].position.y
+        robot_pose_z = ModelStates.pose[MODELSTATE_INDEX].position.z
+
+def subscribe_data_position_error_chan_ho(calc_pose):
+    error_pose = Pose()
+    error_pose.position.x = np.abs(calc_pose.position.x - robot_pose_x)
+    error_pose.position.y = np.abs(calc_pose.position.y - robot_pose_y)
+    pub_chan_ho.publish(error_pose)
+
+def subscribe_data_position_error_harbi(calc_pose):
+    error_pose = Pose()
+    error_pose.position.x = np.abs(calc_pose.position.x - robot_pose_x)
+    error_pose.position.y = np.abs(calc_pose.position.y - robot_pose_y)
+    pub_harbi.publish(error_pose)
+
+if __name__ == "__main__":
+    #get uwb anchors postion
+    #global sensor_pos
+
+    MODELSTATE_INDEX = rospy.get_param('/pozyx_simulation/modelstate_index',2)
+    rospy.loginfo("%s is %s", rospy.resolve_name('/pozyx_simulation/modelstate_index'), MODELSTATE_INDEX)
+
+
+    time.sleep(0.5)
+
+    #get robot real position => you can change ModelStates.pose[] different robot's
+    rospy.Subscriber('gazebo/model_states', ModelStates, subscribe_data_robot_poses)
+
+    rospy.Subscriber('localization_data_chan_ho_topic', Pose, subscribe_data_position_error_chan_ho)
+
+    rospy.Subscriber('localization_data_harbi_topic', Pose, subscribe_data_position_error_harbi)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
---
+++ plotjuggler-ros-plugins/README.md
@@ -0,0 +1,55 @@
+# Link to PlotJuggler ROS Plugins GitHub
+
+This package does not belong to us. Here is the link to the original GitHub for the
+PlotJuggler ROS Plugins GitHub https://github.com/PlotJuggler/plotjuggler-ros-plugins/.
+
+# ROS plugins for PlotJuggler
+
+PlotJuggler works great with ROS, but it is not itself a "ROS" application.
+
+ROS is supported through external plugins that can be found in this [repository](https://github.com/PlotJuggler/plotjuggler-ros-plugins/).
+
+## Existing Plugins
+
+- DataLoader for **rosbags** (ROS / ROS2).
+- ROS **topic subscriber** (ROS / ROS2).
+- **Logs/rosout** visualizer (ROS only).
+- **Re-publisher** similar to `rosbag play` (ROS only).
+
+
+## Install with Debians (TODO)
+
+Install PlotJuggler and its ROS plugins with:
+
+    sudo apt install ros-${ROS_DISTRO}-plotjuggler
+    
+To launch PlotJuggler on ROS, use the command:
+
+      rosrun plotjuggler plotjuggler
+
+or, if have ROS2 installed:
+
+       ros2 run plotjuggler plotjuggler
+
+## How to compile PlotJuggler from source
+
+Create a catkin workspace and clone the repositories:
+
+     mkdir -p ~/ws_plotjuggler/src
+     cd ~/ws_plotjuggler/src
+     git clone https://github.com/PlotJuggler/plotjuggler_msgs.git
+     git clone https://github.com/facontidavide/PlotJuggler.git
+     git clone https://github.com/PlotJuggler/plotjuggler-ros-plugins.git
+     
+Now, it is time to compile:
+
+    cd ~/ws_plotjuggler
+    rosdep install --from-paths src --ignore-src --rosdistro=${ROS_DISTRO} -y
+    catkin build
+    
+Enjoy:
+
+    source devel/setup.bash
+    roslaunch plotjuggler_ros plotjuggler.launch
+    
+     
